74ced6a2adc3ca2c28fa78cae973331d9fdb9ac9
yifantong1010 <yifantong1010@gmail.com>
2024-02-17 12:57:58 -0800

    test pose comparison

---
+++ aframe/func.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+def angle(p1, mid, p2):
+    v1_u = unit_vector(p1 - mid)
+    v2_u = unit_vector(p2 - mid)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
